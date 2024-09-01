f349e286fc9942008ddd1c97b38cb9c8e960b862
dp <dalet.p@gmail.com>
2024-09-01 18:47:27 +0300

    added marker and some fix

--- Testing_framework/Tests/test_example.py
+++ Testing_framework/Tests/test_example.py
@@ -1,7 +1,22 @@
 import pytest
+from Testing_framework.framework.resources.helpers.logger import logger
 
 def somma(a, b):
     return a + b
 
-def test_somma(test_env):
-    assert somma(2, 3) == 5
+class TestExampleClass:
+    @pytest.mark.timeout(600)
+    @pytest.mark.sanity
+    def test_somma(self, test_env):
+        """
+        Verify somma function
+        """
+        def _run_main_flow(test):
+            logger.info(f"Start test Verify Somma ")
+            """
+            MAIN FLOW FUNCTIONS             
+            """
+            assert somma(2, 3) == 5
+
+        _run_main_flow(test_env)
+    
