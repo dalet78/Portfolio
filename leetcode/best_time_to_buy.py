8739e359c139ac6c658132964f8c742a13aaab8a
dp <dalet.p@gmail.com>
2024-09-10 16:27:14 +0300

    added new test best buy

--- leetcode/best_time_to_buy.py
+++ leetcode/best_time_to_buy.py
@@ -5,14 +5,14 @@ class Solution:
     def maxProfit(self) -> int:
         total_profit = 0
         if not self.prices:
-            return total_profit 
+            return 0
         
         for i in range(1, len(self.prices)):
             if self.prices[i] > self.prices[i-1]:
                 total_profit += self.prices[i]- self.prices[i-1] 
-            return total_profit
+        return total_profit
             
 
-test_solution = Solution(prices=[7,1,5,3,6,4])
+test_solution = Solution(prices=[7,6,4,3,1])
 result= test_solution.maxProfit()
 print (result)
